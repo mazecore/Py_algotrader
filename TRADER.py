ab45dbdc74a02364373a5837b2d2c43df5a62043
Ildar Masagutov <masagutov@yahoo.com>
2019-12-30 01:22:31 -0500

    ready for basic buy sell test based on level II alone

--- TRADER.py
+++ TRADER.py
@@ -11,34 +11,38 @@ from bs4 import BeautifulSoup
 
 import requests
 import matplotlib.pyplot as plt
-import numpy
 import pandas as pd  
-from math import *
-import time, datetime
-import threading
+import time
+from datetime import datetime
+from tinydb import TinyDB, Query
+
 
 
 class TRADER:
     
     def __init__(self):
-        print(str(datetime.datetime.now()).split('.')[0])
+        print(str(datetime.now()).split('.')[0])
         self.browser = webdriver.Chrome('C:\chromedriver')
         # self.bidsAreHigh = false
-        self.maCash = 25000
-        self.url = "https://query1.finance.yahoo.com/v8/finance/chart/%5EGSPC?symbol=%5EGSPC&period1={}&period2={}&interval={}&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=ED2zlWJHcMa&corsDomain=finance.yahoo.com"
+        self.yahooFinanceURL = "https://query1.finance.yahoo.com/v8/finance/chart/{0}?symbol={0}&period1={1}&period2={2}&interval={3}&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=ED2zlWJHcMa&corsDomain=finance.yahoo.com"
         self.browser.get('http://markets.cboe.com/us/equities/market_statistics/book_viewer/')
         self.limit_order_pending = False
         self.stock_purchased = False
-        self.changeStock_and_go_to_EDGX('TVIX')
+        self.db = TinyDB('DB.json')
+        record = Query()
+        self.maCash = (self.db.search(record.type == 'current_state'))[0]['cash_amount']
+        print(self.maCash)
+        self.tradeNumber = 0
+        self.change_Stock_and_go_to_EDGX('TVIX')
+        .
 
-    def getYahooData(self):
+    def get_Yahoo_Data(self):
         now = str(time.time()).split('.')[0]
-        print(now)
-        r = requests.get(self.url.format('1574186393', now, '30m'))
-        yahoo = r.json()
-        times = yahoo["chart"]["result"][0]["timestamp"]
-        quote = yahoo["chart"]["result"][0]["indicators"]["quote"][0]
-        # intialise data of lists. 
+        print('getting sp500 data', now)
+        r = requests.get(self.yahooFinanceURL.format('%5EGSPC','1574186393', now, '30m'))
+        sp500 = r.json()
+        times = sp500["chart"]["result"][0]["timestamp"]
+        quote = sp500["chart"]["result"][0]["indicators"]["quote"][0]
         data = {'Open': quote['open'] , 
                 'Low': quote['low'] , 
                 'High': quote['high'], 
@@ -48,8 +52,18 @@ class TRADER:
         df = pd.DataFrame(data) 
         self.moneyFlow = self.MFI(df, 14)
         
-    def changeStock_and_go_to_EDGX(self, stock):
+    def get_daily_Volume(self, stock):
+        print('getting daily volume')
+        now = str(time.time()).split('.')[0]
+        s = str(datetime.today().date())
+        startOfDay = str( time.mktime(datetime.strptime(s, "%Y-%m-%d").timetuple()) ).split('.')[0]
+        r = requests.get(self.yahooFinanceURL.format(stock, startOfDay, now, '1d'))
+        yahooData = r.json()
+        return yahooData["chart"]["result"][0]["indicators"]["quote"][0]['volume']
+        
+    def change_Stock_and_go_to_EDGX(self, stock):
         inputt = self.browser.find_element_by_xpath('//*[@id="symbol0"]')
+        print('switching stock to tvix')
         actions = ActionChains(self.browser)
         actions.move_to_element(inputt)
         actions.double_click(inputt)
@@ -58,6 +72,7 @@ class TRADER:
         actions.send_keys(Keys.ENTER)
         actions.pause(1)
         EDGX = self.browser.find_element_by_xpath('//span[text()="EDGX Equities"]')
+        print('going to EDGX')
         actions.click(EDGX)
         actions.pause(1)
         actions.perform()
@@ -65,9 +80,7 @@ class TRADER:
         self.get_info_table()
 
     def MFI(df, n):
-        """
-        Money Flow Index and Ratio
-        """
+
         PP = (df['High'] + df['Low'] + df['Close']) / 3
         i = 0
         PosMF = [0]
@@ -85,7 +98,7 @@ class TRADER:
 
 
     def set_limit_order(self, price, n):
-        if self.last10TradesPrices[0].text > price:
+        if self.currentPrice > price:
             self.limit_order_pending = True
         else:
             self.buy(price, n)
@@ -93,21 +106,30 @@ class TRADER:
             
             
     def buy(self, stockPrice, n):
-    #     getAmount
-    #     subtract
-    #     keep the amount as a sum of stock value and the remaining cash
-        stocksValue = stockPrice * n
-        self.maCash = self.maCash - stocksValue
+        self.maCash = self.maCash - stockPrice * n
+        self.register_the_trade(n, stockPrice, 'purchase')
         
+    def sell(self, stockPrice, n):
+        self.maCash = self.maCash + stockPrice * n
+        self.register_the_trade(n, stockPrice, 'sale')
+
 
     def check_stock_info(self):
-        if not self.limit_order_pending or not self.stock_purchased:
-            i = 0
-            for j in self.topBidShares:
-                print(j.text)
-               # if j.text > 1600:
-                 #   self.set_limit_order(self.topBidsPrice[i].text, 500)
-                i = i + 1
+        self.currentPrice = float(self.last10TradesPrices[0].text)
+        if not self.limit_order_pending: 
+            if not self.stock_purchased:
+                i = 0
+                for j in self.topBidShares:
+                    if int(j.text) > 1600:
+                        self.set_limit_order(self.topBidsPrice[i].text, 500)
+                    i = i + 1
+            else:
+                b = 0
+                (self.db.search(record.type == 'current_state'))[0]['cash_amount']
+                print(f.read())
+                for s in self.topAskShares:
+                    if int(s.text) >= 1600:
+                        
                     
 
     def get_info_table(self):
@@ -117,27 +139,26 @@ class TRADER:
         self.topAskPrice = self.browser.find_elements_by_class_name("book-viewer__ask-price")
         self.topAskShares = self.browser.find_elements_by_class_name("book-viewer__ask-shares")
         self.set_interval()
-        # button = browser.find_element_by_xpath('//button[text()="I agree"]')
-        # actions = ActionChains(browser)
-        # actions.pause(1)
-    #     actions.move_to_element(button)
-        # print(button)
-    #     actions.click(button)
-        # actions.pause(1)
-        # actions.perform()
-        # bookRaw = browser.find_element_by_xpath('//*[@id="bookViewer0"]/div[1]/table[2]/tbody')
-        
-    #     shares = 
-        # book = bookRaw.get_attribute('innerHTML')
-        # bookSoup = BeautifulSoup(book, 'lxml').html.body
-        # print(price)
-        # print('book ===============>', book)
-        # print('bookSoup ===============>', bookSoup)
+
 
     def set_interval(self):
         while True:
-            time.sleep(1)
+            time.sleep(5)
             self.check_stock_info()
+            
+    def register_the_trade(self, n, stockPrice, transactionType):
+        Trade = Query()
+        self.db.update({'cash_amount': self.maCash, 'stocks': None }, Trade.type == 'current_state')
+        self.db.insert({'type': 'trade', 'stocks': [ {'stock': 'tvix', 'shares': n, 'price': stockPrice } ], 'transaction': transactionType })
+        f = open("trading_log.txt", "a")
+        f.write('<=================trade number %s===================' % self.tradeNumber)
+        f.write('cash is %s' % self.maCash)
+        # f.write('stocks value is %s' % self.maCash)
+        f.write('bought %s tvix at %s' % (n, stockPrice))
+        f.write('current price is %s' % self.last10TradesPrices[0].text)
+        f.write('=====================================>')
+        f.close()
+        self.tradeNumber = self.tradeNumber + 1
             
 if __name__ == "__main__":
     TRADER().__init__()
