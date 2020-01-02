6249d998e8647b045c4a16db7759916eb9a3fe15
Ildar Masagutov <masagutov@yahoo.com>
2020-01-02 00:51:58 -0500

    getting sp500 30m money flow

---
+++ ANALYZER.py
@@ -0,0 +1,88 @@
+
+import requests
+# import matplotlib.pyplot as plt
+import pandas as pd  
+import time
+from datetime import datetime
+from tinydb import TinyDB, Query
+from threading import Thread
+import talib
+
+yahooFinanceURL = "https://query1.finance.yahoo.com/v8/finance/chart/{0}?symbol={0}&period1={1}&period2={2}&interval={3}&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=ED2zlWJHcMa&corsDomain=finance.yahoo.com"
+db = TinyDB('DB.json')
+running = True
+
+
+class ANALYZER:
+    
+    def __init__(self):
+        # self.stock = stock
+        print('analizer initialized...')
+        
+        
+    def get_Yahoo_Data(self, stock, beginning, timerange):
+        now = str(time.time()).split('.')[0]
+        print('getting Yahoo data', now)
+        r = requests.get(yahooFinanceURL.format(stock, beginning, now, timerange))
+        yahoo = r.json()
+        times = yahoo["chart"]["result"][0]["timestamp"]
+        quote = yahoo["chart"]["result"][0]["indicators"]["quote"][0]
+        data = {'Open': quote['open'] , 
+                'Low': quote['low'] , 
+                'High': quote['high'], 
+                'Close': quote['close'],
+                'Volume': quote['volume'],
+                'Timestamp': times }
+        df = pd.DataFrame(data)
+        return df
+        
+        
+    def get_SP500_30minStateEvery15min(self):
+        while running == True:
+            monthAgo = time.time() - 2419200
+            sp_df = self.get_Yahoo_Data('%5EGSPC', str(monthAgo).split('.')[0], '30m')
+            moneyFlow = talib.MFI(sp_df, 14)
+            Trade = Query()
+            db.update({'SP500_30mMF': moneyFlow[-1:]}, Trade.type == 'current_state')
+            time.sleep(900)
+        
+    def slopeOFTheMF(df):
+        for 
+
+        
+    def get_daily_Volume(self, stock):
+        print('getting daily volume')
+        now = str(time.time()).split('.')[0]
+        s = str(datetime.today().date())
+        startOfDay = str( time.mktime(datetime.strptime(s, "%Y-%m-%d").timetuple()) ).split('.')[0]
+        r = requests.get(yahooFinanceURL.format(stock, startOfDay, now, '1d'))
+        yahooData = r.json()
+        return yahooData["chart"]["result"][0]["indicators"]["quote"][0]['volume']
+        
+    
+        
+    # def get_price_action():
+        
+    # def get_20MA():
+        
+    # def get_overall_estimate():
+        
+    # def get_weekly_state():
+        
+    # def get_daily_state():
+        
+    # def get_4hour_state():
+        
+    # def get_1hour_state():
+        
+    # def get_30min_state():
+        
+    # def get_5min_state():
+        
+    # def get_2min_state():
+    
+if __name__=='__main__':
+    a = threading.Thread(target= ANALYZER().get_SP500_30minStateEvery15min)
+    b = threading.Thread(target=ANALYZER().get_daily_Volume, args=('TVIX',))
+    a.start()
+    b.start()
--- talib.py
+++ talib.py
@@ -6,30 +6,30 @@ Created on Mon Dec 30 22:47:39 2019
 """
 import pandas as pd  
 
-class Talib:
-    
-    def MFI(df, n):
 
-        PP = (df['High'] + df['Low'] + df['Close']) / 3
-        i = 0
-        PosMF = [0]
-        while i < len(df) - 1:  # df.index[-1]:
-            if PP[i + 1] > PP[i]:
-                PosMF.append(PP[i + 1] * df.iat[i + 1, df.columns.get_loc('Volume')])
-            else:
-                PosMF.append(0)
-            i=i + 1
-        PosMF = pd.Series(PosMF)
-        TotMF = PP * df['Volume']
-        MFR = pd.Series(PosMF / TotMF)
-        result = pd.Series(MFR.rolling(n).mean(), name='MFI_' + str(n))
-        return result
     
-    def ROC(df, n, price='Close'):
-        """
-        Rate of Change
-        """
-        M = df[price].diff(n - 1)
-        N = df[price].shift(n - 1)
-        result = pd.Series(M / N, name='ROC_' + str(n))
-        return result
+def MFI(df, n):
+
+    PP = (df['High'] + df['Low'] + df['Close']) / 3
+    i = 0
+    PosMF = [0]
+    while i < len(df) - 1:  # df.index[-1]:
+        if PP[i + 1] > PP[i]:
+            PosMF.append(PP[i + 1] * df.iat[i + 1, df.columns.get_loc('Volume')])
+        else:
+            PosMF.append(0)
+        i=i + 1
+    PosMF = pd.Series(PosMF)
+    TotMF = PP * df['Volume']
+    MFR = pd.Series(PosMF / TotMF)
+    result = pd.Series(MFR.rolling(n).mean(), name='MFI_' + str(n))
+    return result
+
+def ROC(df, n, price='Close'):
+    """
+    Rate of Change
+    """
+    M = df[price].diff(n - 1)
+    N = df[price].shift(n - 1)
+    result = pd.Series(M / N, name='ROC_' + str(n))
+    return result
